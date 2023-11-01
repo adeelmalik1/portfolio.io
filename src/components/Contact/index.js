9eb84c4fed5d88a401856482189fe887cde612ef
adeelmalik1 <adeeladi0014@gmail.com>
2023-11-01 15:44:07 -0400

    portfolio updates

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_zovjtzh', 'template_7ecwsnn', form.current, '6k2R9rWtGgP8aVO0A')
       .then((result) => {
         setOpen(true);
         form.current.reset();
@@ -142,7 +142,7 @@ const Contact = () => {
 
 
   return (
-    <Container>
+    <Container id="contact">
       <Wrapper>
         <Title>Contact</Title>
         <Desc>Feel free to reach out to me for any questions or opportunities!</Desc>
